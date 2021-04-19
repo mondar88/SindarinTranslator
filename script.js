d3a6b20c332dab93ef20024a7dce679afcba46d2
mondar88 <52878910+mondar88@users.noreply.github.com>
2021-04-19 14:12:08 +0530

    feat: api deployed

---
+++ script.js
@@ -0,0 +1,36 @@
+var inputTxt = document.querySelector("#input-text");
+var buttonTranslate = document.querySelector("#btnTrans");
+var outputTxt = document.querySelector("#output");
+//input
+function displayfunction(){
+    console.log("now it is clicked!");
+    var urlHead = getURL(inputTxt.value);
+    outputis(urlHead);
+}
+
+//processing
+function getURL(extension){
+    var apiUrl = "https://api.funtranslations.com/translate/sindarin.json";
+    var header = apiUrl+"?"+"text="+extension;
+    console.log(header);
+    return header;
+
+}
+
+//error handling
+function errorHandler(error){
+    console.log("we encountered some error!", error);
+    alert("Seems server is down! come back after a while.")
+}
+
+//output
+function outputis(urlHead){
+    fetch(urlHead)
+    .then(response => response.json())
+    .then(function writeToOutput(json){
+        outputTxt.textContent = json.contents.translated;
+    })
+    .catch(errorHandler)
+}
+
+buttonTranslate.addEventListener("click", displayfunction);
